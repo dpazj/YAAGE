8a6e14ca6cbf937d75745458b9dc0b6569efc584
Joe Riemersma <joepaz200@gmail.com>
2021-01-01 17:29:54 +0000

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -6,39 +6,7 @@
 #include <iostream>
 #include <iomanip>
 
-#include "nuuue/nue.h"
-
-// void SanityTest()
-// {
-//     Value x({-4.0});
-//     Value two({2.0});
-//     //z
-//     Mul x_mul_two(x,two);
-//     Add x_add_two(x,two);
-//     Add z(x_mul_two, x_add_two); 
-
-//     //q
-//     ReLU z_relu(z);
-//     Mul z_mul_x(z,x);
-//     Add q(z_mul_x, z_relu);
-
-//     //h
-//     Mul z_mul_z(z,z);
-//     ReLU h(z_mul_z);
-
-//     //y
-//     Mul q_mul_x(q,x);
-//     Add q_add_qmulx(q_mul_x,q);
-//     Add y(q_add_qmulx, h);
-  
-//     Graph graph(x,y);
-//     graph.Forward();
-//     auto* answer = graph.Forward();
-//     std::cout << "y should be -20, y = ";   answer->Print();
-//     auto* gradient_input = graph.Backward();
-//     std::cout << "dy/dx should be 46, dy/dx = "; gradient_input->Print();
-    
-// }
+#include "cozygrad/cozygrad.h"
 
 
 void sanity_test()
