ec24b5acbab03701cba45768852f5591efb02fcb
Joe Riemersma <joepaz200@gmail.com>
2021-01-11 16:13:11 +0000

    init

--- main.cpp
+++ main.cpp
@@ -59,65 +59,6 @@ void test2()
     x.gradient()->print();
 }
 
-class MoonNet : public model
-{
-    node& create_model()
-    {
-        auto& input = create_input_node();
-
-        auto& w1 = create_model_param(2,16);
-        auto& w2 = create_model_param(16,16);
-        auto& w3 = create_model_param(16,1);
-
-        auto& b1 = create_model_param(1,16);
-        auto& b2 = create_model_param(1,16);
-        auto& b3 = create_model_param(1,1);
-
-        auto& l1 = (input.dot(w1) + b1).relu(); //layer 1
-        auto& l2 = (l1.dot(w2) + b2).relu(); //layer 2
-        auto& l3 = (l2.dot(w3) + b3).sigmoid(); //layer 3
-        
-        return l3;
-    }
-};
-
-std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    std::stringstream ss(s);
-    std::string item;
-    while(std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-    return elems;
-}
-
-void do_moon()
-{
-    std::vector<tensor> X;
-    std::vector<tensor> y;
-
-    std::ifstream moon("./datasets/moon_dataset.txt");
-    std::string line;
-    while (std::getline(moon, line))
-    {
-        auto vals = split(line, ',');
-        tensor tmp = {atof(vals[0].c_str()), atof(vals[1].c_str())};
-        tensor tmp1 = {atof(vals[2].c_str())};
-        X.push_back(tmp);
-        y.push_back(tmp1); 
-    }
-
-    double learning_rate = 0.05;
-    unsigned int epoch = 5;
-    SDG optim(learning_rate);
-    MoonNet model;
-    model.train(X,y, optim, epoch, loss::binary_cross_entropy);
-    model.evaluate(X,y);
-}
-
-
-
-
 void test3()
 {
     tensor a = {0,0,0,0,0,1,0,0,0,0};
@@ -136,7 +77,6 @@ void test3()
 
     z.data()->print();
     yh.gradient()->print();
-
 }
 
 int main()
@@ -145,10 +85,7 @@ int main()
     //test1();
     //test2();
     //test3();
-    //do_moon();
-
-    do_mnist();
-
+ 
     utils::clean_session();
 
     return 0;
